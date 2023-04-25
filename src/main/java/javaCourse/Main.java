fbd5e393e6fddd9580f0489aa4301a0350c69ff9
darecki9k <darecki9k@gmail.com>
2023-04-25 14:08:09 +0200

    Rename the package. Adding your own class file MyHello.

--- src/main/java/javaCourse/Main.java
+++ src/main/java/javaCourse/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package javaCourse;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/javaCourse/MojeHello.java
@@ -0,0 +1,7 @@
+package javaCourse;
+
+public class MojeHello {
+    public static void main(String[] args) {
+        System.out.println("Moje hello");
+    }
+}

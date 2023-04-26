552f3cd7c77ed1554cf7dc6b11ae1b55b16e5ed3
darecki9k <darecki9k@gmail.com>
2023-04-26 10:40:38 +0200

    Static methods on String. Methods on string instances.

--- src/main/java/javaCourse/BMICalc.java
+++ src/main/java/javaCourse/BMICalc.java
@@ -2,21 +2,50 @@ package javaCourse;
 
 public class BMICalc {
     public static void main(String[] args) {
-        //BMI = weight in kg / (height in meters)^2
-        //1. two integer variables: weight and height in cm and assign some values
-        // calculate BMI and print to screen.
-        int weight = 90;
-        int height = 188;
-        double heightInMeters = height / 100.0;
+        //declaration
+        String name = "Darek";
+        String lastName = "Zegarek";
 
-        double bmi = weight / heightInMeters / heightInMeters;
-        System.out.println(bmi);
+        int age = 41;
 
-        //2. two floating point variables weight and height and rewrite some values.
-        float weight2 = 89.6f;
-        float height2 = 1.88f;
-        float bmi2 = weight2 / height2 / height2;
-        System.out.println(bmi2);
+        System.out.println(name);
+
+        //combining variables
+        String nameAndLastName = name + " " + lastName;
+        System.out.println(nameAndLastName);
+
+        System.out.println(nameAndLastName + age);
+
+        //overwriting
+        name = "Adam";
+        System.out.println(nameAndLastName);
+
+        //methods
+        int length = name.length();
+        System.out.println(length);
+        System.out.println(name.toUpperCase());
+        // also primitive type
+        char c = name.charAt(1);
+        System.out.println(c);
+
+        //converting a number to a string
+        String ageAsString = String.valueOf(age);
+
+        System.out.println(ageAsString);
+
+        int age41 = Integer.parseInt(ageAsString);
+        System.out.println(ageAsString);
+
+        //Task.
+        //Create a String variable, assign your name to it,
+        // write it in lower case.
+        // Change the first letter to uppercase and display on the screen.
+        String myName = "darek";
+        char firstLetter = myName.charAt(0);
+        //String firstLetterAsStr = String.valueOf(firstLetter);
+        //String upperCaseFirstLetter = firstLetterAsStr.toUpperCase();
+        char upperCaseFirst = Character.toUpperCase(firstLetter);
+        System.out.println(upperCaseFirst + myName.substring(1));
 
     }
 }

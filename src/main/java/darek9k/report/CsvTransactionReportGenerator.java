f412991fe5c3228aaee2f202fe08cf4d959c77a2
darecki9k <darecki9k@gmail.com>
2023-05-13 05:20:40 +0200

    common csv from Apache

--- src/main/java/darek9k/report/CsvTransactionReportGenerator.java
+++ src/main/java/darek9k/report/CsvTransactionReportGenerator.java
@@ -1,5 +1,10 @@
 package darek9k.report;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 class CsvTransactionReportGenerator implements TransactionReportGenerator {
@@ -7,6 +12,19 @@ class CsvTransactionReportGenerator implements TransactionReportGenerator {
     @Override
     public void generate(List<TransactionsReportLine> transactionsReportLines) {
         System.out.println("Ja generuje CSV");
+
+        try (CSVPrinter printer = new CSVPrinter(new FileWriter("csv.txt"), CSVFormat.EXCEL)) {
+            for (TransactionsReportLine transactionsReportLine : transactionsReportLines) {
+                printer.printRecord(transactionsReportLine.getId(),
+                        transactionsReportLine.getCustomerName(),
+                        transactionsReportLine.getProductName(),
+                        transactionsReportLine.getDate(),
+                        transactionsReportLine.getQuantity(),
+                        transactionsReportLine.getPrice());
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
         transactionsReportLines.forEach(transactionsReportLine -> System.out.println(transactionsReportLine.toString()));
     }
--- src/main/java/darek9k/report/TransactionsReportLine.java
+++ src/main/java/darek9k/report/TransactionsReportLine.java
@@ -1,12 +1,9 @@
 package darek9k.report;
 
-import lombok.ToString;
-import lombok.Value;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Value
+
 class TransactionsReportLine {
      Integer id;
      String customerName;
@@ -14,5 +11,50 @@ class TransactionsReportLine {
      LocalDate date;
      Integer quantity;
      BigDecimal price;
+
+     public Integer getId() {
+          return id;
+     }
+
+     public String getCustomerName() {
+          return customerName;
+     }
+
+     public String getProductName() {
+          return productName;
+     }
+
+     public LocalDate getDate() {
+          return date;
+     }
+
+     public Integer getQuantity() {
+          return quantity;
+     }
+
+     public BigDecimal getPrice() {
+          return price;
+     }
+
+     public TransactionsReportLine(Integer id, String customerName, String productName, LocalDate date, Integer quantity, BigDecimal price) {
+          this.id = id;
+          this.customerName = customerName;
+          this.productName = productName;
+          this.date = date;
+          this.quantity = quantity;
+          this.price = price;
+     }
+
+     @Override
+     public String toString() {
+          return "TransactionsReportLine{" +
+                  "id=" + id +
+                  ", customerName='" + customerName + '\'' +
+                  ", productName='" + productName + '\'' +
+                  ", date=" + date +
+                  ", quantity=" + quantity +
+                  ", price=" + price +
+                  '}';
+     }
 }
 
